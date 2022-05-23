7b7a597d69febb07531181df494b37ddb39479e9
afernander <afernander@eafit.edu.co>
2022-05-23 16:34:05 -0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.use(cookieSession({
     keys: ['asdf']
   }))
